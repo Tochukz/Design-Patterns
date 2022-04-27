12554fb1778c103d2e192f54ece7e342a5edd8cc
Tochukwu Nwachukwu <truetochukz@gmail.com>
2022-04-27 10:06:13 +0200

    Factory Pattern

--- CreationalPatterns/FactoryPattern/FactoryPattern.java
+++ CreationalPatterns/FactoryPattern/FactoryPattern.java
@@ -14,3 +14,20 @@ public class FactoryPattern {
         rectangle.draw();
     }
 }
+
+/**
+FactoryPattern
+--------------
+Instead of creating an object directly using the new operator, we have a Factory class that generate the directed object for use when needed.
+
+Implementation 
+--------------
+We have Circle, Square and Rectangle classes which implement the Shape interface.  
+We alsohave the ShapeFactory with the method ShapeFactory.getShape which instantiate and return the desired object.
+
+Output
+--------- 
+  Circle drawn
+  Square drawn
+  Rectangle drawn
+*/
